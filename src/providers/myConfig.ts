100fba0f447a3858d7a8144bf35a7e38245825a2
Your Name <you@example.com>
2019-04-16 08:25:44 +0700

    A

--- src/providers/myConfig.ts
+++ src/providers/myConfig.ts
@@ -1,5 +1,5 @@
 export let data = {
-    "url" : "http://0.0.0.0:58056"
-    //"url" : "https://thebesttoken.co"
+    //"url" : "http://0.0.0.0:58056"
+    "url" : "https://thebesttoken.co"
     
 }
